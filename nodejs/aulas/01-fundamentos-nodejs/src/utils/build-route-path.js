b5d7f16bb421976d53b0a217a5d388420bcf2647
Gustavo Sabino <gu.sabino@hotmail.com>
2024-11-23 19:13:01 -0300

    Rotas da aplicação - Rotas com parâmetros (Regex)

--- nodejs/aulas/01-fundamentos-nodejs/src/utils/build-route-path.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/utils/build-route-path.js
@@ -1,7 +1,11 @@
 // /users/:id
 
 export function buildRoutePath(path) {
-  const routeParametersRegex = /:([a-zA-Z]+)/g
+  const routeParametersRegex = /:([a-zA-Z]+)/g//Buscar e verificar se existe parâmetro dinâmico
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-  console.log(Array.from(path.matchAll(routeParametersRegex)))
+  const pathRegex = new RegExp(`^${pathWithParams}`)
+
+
+  return pathRegex
 }
