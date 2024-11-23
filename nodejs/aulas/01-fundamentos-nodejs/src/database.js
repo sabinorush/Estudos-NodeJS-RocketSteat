d215a81b0fcd2700c18a00e74ada9ba2c2c10a77
Gustavo Sabino <gu.sabino@hotmail.com>
2024-11-23 05:40:41 -0300

    Banco de dados - Persistindo bando de dados

--- nodejs/aulas/01-fundamentos-nodejs/src/database.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/database.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../db.json', import.meta.url)
+
 export class Database {
   #database = {}
+
+  constructor() { //Método construtor para ler os dados recebidos
+    fs.readFile(databasePath, 'utf-8')
+      .then(data => {
+      this.#database = JSON.parse(data)
+    })
+    .catch(() => {
+      this.#persist()
+    })
+  }
+
+  #persist() { //Persistindo os dados do banco de dados
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   select(table) {
     const data = this.#database[table] ?? []
@@ -13,6 +31,8 @@ export class Database {
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist();
 
     return data;
   }
