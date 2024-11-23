953cdf29c190a017e3c15ea035eb139dd7bd85b5
Gustavo Sabino <gu.sabino@hotmail.com>
2024-11-23 05:02:11 -0300

    Streams no Node.js - Entendendo Buffers

---
+++ nodejs/aulas/01-fundamentos-nodejs/streams/buffer.js
@@ -0,0 +1,7 @@
+//buffer => Representação de um espaço na memória do computador;
+// Itens que serão armazenados e depois removidos;
+// Buffer é uma API criada dentro do node para trabalhar com dados binários;
+
+const buf = Buffer.from("Olá")
+
+console.log(buf)
