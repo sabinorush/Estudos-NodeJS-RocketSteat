f2a3497d8afb10f2e55ae51a456053b6821241c9
Gustavo Sabino <gu.sabino@hotmail.com>
2024-11-25 16:10:18 -0300

    Rotas da aplicação - Filtrando lista do bancos de dados

--- nodejs/aulas/01-fundamentos-nodejs/src/routes.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/routes.js
@@ -12,9 +12,14 @@ export const routes = [
     method: 'GET',
     path: buildRoutePath('/users'),
     handler: (req, res) => {
-      const users = database.select('users')
-      console.log(req.query)
+      const { search } = req.query
 
+
+      const users = database.select('users', search ?  {
+        name: search,
+        email: search
+      } : null)
+  
       return res.end(JSON.stringify(users))
     }
   },
